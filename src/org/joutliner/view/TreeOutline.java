8efc7dde68fc22f65fdca94f3a81f47f27bb6657
Richard Varela <richardvare13@gmail.com>
2025-07-06 11:52:28 -0700

    Rewrote the code in TreeOutline from what the tutorial had. Used inversion to fix the nested if issue.

--- src/org/joutliner/view/TreeOutline.java
+++ src/org/joutliner/view/TreeOutline.java
@@ -9,9 +9,9 @@ import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
 
 public class TreeOutline extends JPanel {
-	protected DefaultMutableTreeNode rootNode;
-	protected DefaultTreeModel treeModel;
-	protected JTree tree;
+	private DefaultMutableTreeNode rootNode;
+	private DefaultTreeModel treeModel;
+	private JTree tree;
 	private Toolkit toolkit = Toolkit.getDefaultToolkit();
 
 	public TreeOutline() {
@@ -22,34 +22,39 @@ public class TreeOutline extends JPanel {
 		this.tree = new JTree(this.treeModel);
 		this.tree.setEditable(true);
 		this.tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-		tree.setShowsRootHandles(true);
+		this.tree.setShowsRootHandles(true);
 
-		this.add(new JScrollPane(tree));
+		this.add(new JScrollPane(this.tree));
 	}
 
-	public void removeSelectedNode() { // TODO: Remove nesting
-		TreePath currentSelection = tree.getSelectionPath();
-		if (currentSelection != null) {
-			DefaultMutableTreeNode currentNode = (DefaultMutableTreeNode)(currentSelection.getLastPathComponent());
-			MutableTreeNode parent = (MutableTreeNode)(currentNode.getParent());
-			if (parent != null) {
-				treeModel.removeNodeFromParent(currentNode);
-				return;
-			}
+	public void removeSelectedNode() {
+		TreePath currentSelection = this.tree.getSelectionPath();
+
+		if (currentSelection == null) { // Nothing selected
+			this.toolkit.beep();
+			return;
 		}
 
-		toolkit.beep(); // No selection or root was selected
+		DefaultMutableTreeNode currentNode = (DefaultMutableTreeNode)currentSelection.getLastPathComponent();
+		MutableTreeNode parent = (MutableTreeNode)currentNode.getParent();
+
+		if (parent == null) { // Root node selected
+			this.toolkit.beep();
+			return;
+		}
+
+		this.treeModel.removeNodeFromParent(currentNode);
 	}
 
 	public DefaultMutableTreeNode addObject(Object child) {
 		DefaultMutableTreeNode parentNode = null;
-		TreePath parentPath = tree.getSelectionPath();
+		TreePath parentPath = this.tree.getSelectionPath();
 
 		if (parentPath == null) {
-			parentNode = rootNode;
+			parentNode = this.rootNode;
 		}
 		else {
-			parentNode = (DefaultMutableTreeNode)(parentPath.getLastPathComponent());
+			parentNode = (DefaultMutableTreeNode)parentPath.getLastPathComponent();
 		}
 
 		return addObject(parentNode, child, true);
@@ -63,26 +68,30 @@ public class TreeOutline extends JPanel {
 		DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(child);
 
 		if (parent == null) {
-			parent = rootNode;
+			parent = this.rootNode;
 		}
 
-		treeModel.insertNodeInto(childNode, parent, parent.getChildCount());
+		this.treeModel.insertNodeInto(childNode, parent, parent.getChildCount());
 
 		if (shouldBeVisible) {
-			tree.scrollPathToVisible(new TreePath(childNode.getPath()));
+			this.tree.scrollPathToVisible(new TreePath(childNode.getPath()));
 		}
+
 		return childNode;
 	}
 
-	class MyTreeModelListener implements TreeModelListener {
-		public void treeNodesChanged(TreeModelEvent e) {
+	// Is this needed?
+	// Renamed to TreeModelHandler; not sure if that makes sense.
+	class TreeModelHandler implements TreeModelListener {
+		public void treeNodesChanged(TreeModelEvent evt) {
+			System.out.println("in MyTreeModelListener");
 			DefaultMutableTreeNode node;
-			node = (DefaultMutableTreeNode)(e.getTreePath().getLastPathComponent());
-			int index = e.getChildIndices()[0];
-			node = (DefaultMutableTreeNode)(node.getChildAt(index));
+			node = (DefaultMutableTreeNode)evt.getTreePath().getLastPathComponent();
+			int index = evt.getChildIndices()[0];
+			node = (DefaultMutableTreeNode)node.getChildAt(index);
         }
-		public void treeNodesInserted(TreeModelEvent e) {}
-		public void treeNodesRemoved(TreeModelEvent e) {}
-		public void treeStructureChanged(TreeModelEvent e) {}
+		public void treeNodesInserted(TreeModelEvent evt) {}
+		public void treeNodesRemoved(TreeModelEvent evt) {}
+		public void treeStructureChanged(TreeModelEvent evt) {}
 	}
 }
